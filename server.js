5ded0053bc5c930a1abfe30be0818099cae34055
Criste Phillips <criste.phillips@yahoo.com>
2022-02-07 18:24:49 -0500

    use tested

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 // import exress
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express;
+const app = express();
 
 // Express middleware
-app.use(expres.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// create GET test route
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+
+
+// KEEP as LAST route prior to listen event; route to handle user requests that are not supported by the app; Default responses for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
 });
 
+
 // starts Express.js server on port 3001
-app.listener(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
