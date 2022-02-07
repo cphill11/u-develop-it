43433062aad4a8900701dbe7c6d10200fa0329de
Criste Phillips <criste.phillips@yahoo.com>
2022-02-07 18:36:06 -0500

    callback for rows

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // import exress
 const express = require('express');
+
+// import mysql2
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,9 +10,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to mysql db
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
-// KEEP as LAST route prior to listen event; route to handle user requests that are not supported by the app; Default responses for any other request (Not Found)
+// runs query method, executes callback w/ all resulting rows that match the query
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+// KEEP as LAST route prior to listen event; CATCHALL route; route to handle user requests that are not supported by the app; Default responses for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
