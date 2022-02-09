6b2cfa3be43530f28b90230a98d340de14525702
Criste Phillips <criste.phillips@yahoo.com>
2022-02-09 14:00:28 -0500

    post req added to voterRoutes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -5,7 +5,7 @@ const inputCheck = require('../../utils/inputCheck');
 
 // GET route for voters (performs SELECT * FROM voters)
 router.get('/voters', (req, res) => {
-    const sql = `SELECT * FROM voters`;
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -17,5 +17,45 @@ router.get('/voters', (req, res) => {
         });
     });
 });
+
+// Get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// POST request to allow ppl to register through the app
+router.post('/voter', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+       if (errors) {
+       res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+        return;
+        }
+        res.json({
+          message: 'success',
+          data: body
+        });
+      });
+    });
 
 module.exports = router;
